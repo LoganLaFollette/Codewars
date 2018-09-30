16a8e0e4c30285225cce6371f175d37db7d65645
llafollette <llafollette@mail.bradley.edu>
2018-09-30 16:01:16 -0500

    driver updates

--- codewars/sandbox.java
+++ codewars/sandbox.java
@@ -11,17 +11,17 @@ public class sandbox {
 		boolean debug = true;
 		//System.out.println(ASum.findNb(m, debug));
 		
-//		System.out.println(SumDigPower.sumDigPow(1, 100));
-//		List<Long> list = SumDigPower.sumDigPow(1, 100);
-//		for (long i : list) {
-//			System.out.println(i + ", ");
-//		}
+		System.out.println(SumDigPower.sumDigPow(1, 100));
+		List<Long> list = SumDigPower.sumDigPow(1, 100);
+		for (long i : list) {
+			System.out.println(i + ", ");
+		}
 		
-//		int[] sorted = mySortOdd.sortArray(new int[]{ 5, 3, 1, 8, 0 });
-//		
-//		for (int i : sorted) {
-//			System.out.print(i + " ");
-//		}
+		int[] sorted = mySortOdd.sortArray(new int[]{ 5, 3, 1, 8, 0 });
+		
+		for (int i : sorted) {
+			System.out.print(i + " ");
+		}
 		String zoo = "bug,grass,busker,bug,cow,grass,lion,leaves,panda,bicycle,little-fish,lion";
 		Dinglemouse.whoEatsWho(zoo);
 	}
